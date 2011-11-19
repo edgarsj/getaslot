aee15c212b6e71285b422d0ddca01f7d6087c234
Ameriks <ameriks@gmail.com>
2011-11-19 14:05:29 +0200

    Static

--- getaslot/views.py
+++ getaslot/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+#from django.conf import settings
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+#	print settings.STATIC_ROOT
+	return render_to_response("index.html", {}, context_instance=RequestContext(request))
