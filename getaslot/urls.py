d81c314442af41d46fb3a46af833dd1c18e18214
Edgars Jekabsons <edgars.jekabsons@gmail.com>
2011-11-20 00:21:08 +0200

    busy schedule is busy

--- getaslot/urls.py
+++ getaslot/urls.py
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r"^(?P<slug>[\w-]+)/$", 'slots.views.business', name='business'),
     url(r"^(?P<business>[\w-]+)/appointments/$", 'slots.views.appointments', name="business_appointments"),
-	url(r"^(?P<business>[\w-]+)/schedule/$", 'slots.views.schedule',{'employee_id':'1'}, name="business_schedule"),
-    url(r"^(?P<business>[\w-]+)/schedule/(?P<employee_id>\d+)$", 'slots.views.schedule', name="business_schedule_employee"),
+	url(r"^(?P<business>[\w-]+)/schedule/$", 'slots.views.busy',{'employee_id':'1'}, name="business_schedule"),
+    url(r"^(?P<business>[\w-]+)/schedule/(?P<employee_id>\d+)$", 'slots.views.busy', name="business_schedule_employee"),
     url(r"^(?P<business>[\w-]+)/add_appointment/(?P<employee_id>\d+)$", 'slots.views.add_appointment_noschedule', name="add_appointment_employee"),
 )
 
