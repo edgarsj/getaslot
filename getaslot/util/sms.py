a0e08cc193dd48924beb7baf2d4a0be38680f904
Edgars Jekabsons <edgars.jekabsons@gmail.com>
2011-11-20 12:36:01 +0200

    send smsmsms

--- getaslot/util/sms.py
+++ getaslot/util/sms.py
@@ -1,4 +1,8 @@
 import urllib  
+import datetime
+
+from slots.models import Appointment
+
 #import urllib2 
 
 from django.conf import settings
@@ -26,3 +30,20 @@ def send_sms(reciever, msg):
     api_url = api_url + '?' + data  
     data = urllib.urlopen(api_url).read()  
     return data  
+
+def send_confirmations():
+    #start_datetime = datetime.datetime.now() - datetime.timedelta(0,600,0)
+    obj_list = Appointment.objects.filter(confirmation_sent=False)
+    for o in obj_list:
+        print o
+        if o.phone.startswith('+3712') or (len(o.phone)==8 and o.phone.startswith('2')):
+            if len(o.phone) ==8:
+                o.phone = '+371'+o.phone
+            msg = u'Henry will be waiting for you at Alexandra Beauty on %s at %s' % (
+                                o.starttime.strftime(u'%Y-%m-%d'),
+                                o.starttime.strftime(u'%H:%M'),
+                                )
+            
+            data = send_sms(o.phone, msg)
+        o.confirmation_sent = True
+        o.save()
