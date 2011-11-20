d95bbcead9905f58e6f88d2375174a8661c7895f
Edgars Jekabsons <edgars.jekabsons@gmail.com>
2011-11-20 11:30:14 +0200

    sms send & model

--- getaslot/util/sms.py
+++ getaslot/util/sms.py
@@ -5,13 +5,24 @@ from django.conf import settings
 
 def send_sms(reciever, msg):  
   
-    api_url = 'https://messente.com/api/send_sms_get'  
-    values = {'user' : settings.MESSENTE_USERNAME,  
-            'api_key' : settings.MESSENTE_API_KEY,  
-            'text' : msg,  
-            'to' : reciever}  
+    ss='http://adforte.com/jovi.member.app-user-commands?action=send&user=agris.ameriks@ameri.lv&password=u@m3d-M1ySyV&phone=+37126461101&sender=GetASlot&text=You'
+    #api_url = 'https://messente.com/api/send_sms_get'
+    #  values = {'user' : settings.MESSENTE_USERNAME,  
+#            'api_key' : settings.MESSENTE_API_KEY,  
+#            'text' : msg,  
+#            'to' : reciever}  
+#    
     
+    api_url = 'http://adforte.com/jovi.member.app-user-commands'
+    values = { 
+              'action' : 'send',
+              'user' : 'agris.ameriks@ameri.lv',
+              'password' : 'u@m3d-M1ySyV',
+              'sender' : 'GetASlot',
+              'phone' : reciever,
+              'text' : msg
+              }
     data = urllib.urlencode(values)  
     api_url = api_url + '?' + data  
     data = urllib.urlopen(api_url).read()  
-    #print data  
+    return data  
